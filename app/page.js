0606c9b7cbf9c1cfd8754706fc0a2cad1046c3e2
Kristian Numme <95479930+Kristiangit@users.noreply.github.com>
2024-02-09 22:58:12 +0100

    chnges

--- app/page.js
+++ app/page.js
@@ -1,15 +1,16 @@
-import Image from "next/image";
 
 export default function Home() {
-  return (
-    
-    <div>
-
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-    </div>
-
-
-  );
-}
+    return (
+      
+      <div className="flex min-h-screen flex-col items-center justify-between p-24">
+        <div className="w-100 h-40 bg-violet-900">
+          <h1 className="text-3xl font-bold underline">KRISTIAN</h1>
+        </div>
+        <h1 className="text-3xl font-bold underline-offset-1 text-red-500">
+          Hello world!
+        </h1>
+      </div>
+  
+    );
+  }
+  
